4c8d90b00cd347b9ed0c4d739378be3e5c1cd0ae
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-30 17:41:07 +0530

    filter and other changes

--- src/javascripts/script.js
+++ src/javascripts/script.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollToPlugin);
 function loco() {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -29,66 +30,81 @@ function loco() {
     ScrollTrigger.refresh();
 
 }
-loco()
+// loco()
 
-function homeLoader(){
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
+window.onload = function () {
+    // Scroll to the top
+    lenis.scrollTo(0, {
+        duration: 0,  // Instant scroll to the top
+        smooth: true, // Smooth scroll behavior
+    });
+};
+
+function homeLoader() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const mainFilter = urlParams.get('mainFilter');
+    if (mainFilter) return;
     var loader = gsap.timeline()
-loader
-.from(".upper", {
-    opacity: 0,
-    duration: .8,
-    timingFunction: "ease-in-out",
-    delay: 1.5,
-    stagger: {
-        from: "center",
-        amount: .2
-    }
-}, "a")
+    loader
+        .from(".upper", {
+            opacity: 0,
+            duration: .8,
+            timingFunction: "ease-in-out",
+            delay: 1.5,
+            stagger: {
+                from: "center",
+                amount: .2
+            }
+        }, "a")
 
-.from(".lower", {
-    opacity: 0,
-    duration: .8,
-    timingFunction: "ease-in-out",
-    stagger: {
-        amount: .2
-    },
-    delay: 1.6,
-}, "a")
+        .from(".lower", {
+            opacity: 0,
+            duration: .8,
+            timingFunction: "ease-in-out",
+            stagger: {
+                amount: .2
+            },
+            delay: 1.6,
+        }, "a")
 
-.from("#main", {
-    backgroundColor: "#F5E31A",
-    duration: .8,
-    timingFunction: "ease-in-out",
-}, "b")
+        .from("#main", {
+            backgroundColor: "#F5E31A",
+            duration: .8,
+            timingFunction: "ease-in-out",
+        }, "b")
 
-.from(".header-text", {
-    bottom: "0%",
-    duration: .8,
-    ease: "power3.out",
-}, "b")
+        .from(".header-text", {
+            bottom: "0%",
+            duration: .8,
+            ease: "power3.out",
+        }, "b")
 
-.from("#page2", {
-    opacity: 0,
-    y: 10,
-    duration: 1,
-    delay:-.5,
-    ease: "power3.out",
-}, "c")
+        .from("#page2", {
+            opacity: 0,
+            y: 10,
+            duration: 1,
+            delay: -.5,
+            ease: "power3.out",
+        }, "c")
 
-.from("#filter-container", {
-    opacity: 0,
-    y: 10,
-    duration: 1,
-    delay:-.5,
-    ease: "power3.out",
-}, "c")
+        .from("#filter-container", {
+            opacity: 0,
+            y: 10,
+            duration: 1,
+            delay: -.5,
+            ease: "power3.out",
+        }, "c")
 
-.from("nav", {
-    opacity: 0,
-    duration: 1,
-    delay:-.5,
-    ease: "power3.out",
-}, "c")
+        .from("nav", {
+            opacity: 0,
+            duration: 1,
+            delay: -.5,
+            ease: "power3.out",
+        }, "c")
 }
 homeLoader()
 
@@ -100,8 +116,8 @@ function serviceAnimation() {
     var previousLink = ""
 
     document.querySelector("#nav-service").addEventListener("click", function () {
-        document.querySelectorAll("nav p").forEach(function(link){
-            if(link.classList.contains("active")){
+        document.querySelectorAll("nav p").forEach(function (link) {
+            if (link.classList.contains("active")) {
                 previousLink = link.textContent.toLowerCase()
             }
         })
@@ -111,8 +127,8 @@ function serviceAnimation() {
     document.querySelector("#closeService").addEventListener("click", function () {
         isService = !isService;
         openService()
-        document.querySelectorAll("nav p").forEach(function(link){
-            if(link.textContent.toLowerCase() === previousLink){
+        document.querySelectorAll("nav p").forEach(function (link) {
+            if (link.textContent.toLowerCase() === previousLink) {
                 link.classList.add("active")
             }
         })
@@ -149,6 +165,48 @@ function serviceAnimation() {
 }
 serviceAnimation()
 
+var currentCategory = "#featured-project"
+
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const mainFilter = urlParams.get('mainFilter');
+
+    // If mainFilter exists
+    if (mainFilter) {
+        // Fade out the #featured-project element
+
+        currentCategory = "#all-project"
+        var allFilter = document.querySelectorAll("#filter-container p")
+        allFilter.forEach(function (filter) {
+            filter.classList.remove("active");
+            if (filter.textContent.toLowerCase() === "all") {
+                filter.classList.add("active");
+            }
+        })
+        var navLink = document.querySelectorAll("nav p")
+        navLink.forEach(function (link) {
+                link.classList.remove("active");
+        })
+        document.querySelector("#nav-project").classList.add("active");
+
+
+        document.querySelector("#featured-project").style.opacity = 0;
+
+        // Use setTimeout to delay hiding it until opacity transition is complete
+        setTimeout(function () {
+            // Hide #featured-project after it fades out
+            document.querySelector("#featured-project").style.display = 'none';
+
+            // Show #all-project
+            const allProject = document.querySelector("#all-project");
+            allProject.style.display = 'flex'; // Show it immediately
+            setTimeout(() => {
+                allProject.style.opacity = 1; // Fade it in
+            }, 50); // Delay for smooth transition
+        }, 500); // Match the duration of opacity transition
+    }
+};
+
 
 function filterAnimation() {
     const allFilter = document.querySelectorAll("#filter-container p")
@@ -157,6 +215,13 @@ function filterAnimation() {
             allFilter.forEach(function (item) {
                 item.classList.remove("active");
             });
+            if (this.textContent.toLowerCase() === "all") {
+                document.querySelector("#nav-project").classList.add("active");
+            }
+            else {
+                document.querySelector("#nav-project").classList.remove("active");
+                history.pushState(null, '', '/');
+            }
             this.classList.add("active");
         })
     })
@@ -165,7 +230,7 @@ function filterAnimation() {
         allFilter.forEach(function (item) {
             item.classList.remove("active");
         });
-        allFilter[allFilter.length -1].classList.add("active");
+        allFilter[allFilter.length - 1].classList.add("active");
     })
     const navLink = document.querySelectorAll("nav p")
     navLink.forEach(function (link) {
@@ -176,9 +241,106 @@ function filterAnimation() {
             this.classList.add("active");
         })
     })
-    
+
+    // ALL PROJECT FILTER
+    document.querySelectorAll(".all-project-button").forEach(function (btn) {
+        btn.addEventListener("click", function () {
+
+            history.pushState(null, '', '/?mainFilter=all');
+            const urlParams = new URLSearchParams(window.location.search);
+            const mainFilter = urlParams.get('mainFilter');
+
+
+            const page2Height = document.querySelector("#page2").getBoundingClientRect().top
+            const currentContainer = document.querySelector(currentCategory)
+            const projectContainer = document.querySelector("#all-project")
+
+            if (currentCategory === '#all-project') {
+                gsap.to(window, {
+                    scrollTo: { y: 0, autoKill: false },
+                    duration: 1
+                })
+                return; // Exit early, skipping the timeline
+            }
+
+            var tl = gsap.timeline()
+            tl
+                .to(currentContainer, {
+                    onStart: function () {
+                        gsap.to(window, {
+                            scrollTo: { y: 0, autoKill: false },
+                            duration: 1
+                        })
+                    },
+                    opacity: 0,
+                    ease: "power3.out",
+                    duration: .5,
+                    onComplete: function () {
+                        gsap.set(currentContainer, { display: "none" })
+                    }
+                })
+                .to(projectContainer, {
+                    opacity: 1,
+                    ease: "power3.out",
+                    duration: .5,
+                    onStart: function () {
+                        gsap.set(projectContainer, { display: "flex" })
+                    },
+                    onComplete: function () {
+                        currentCategory = "#all-project"
+                    }
+                })
+        })
+    })
+
+    //FEATURED PROJECT FILTER
+    document.querySelector("#featured-btn").addEventListener("click", function () {
+
+        history.pushState(null, '', '/');
+
+        const page2Height = document.querySelector("#page2").getBoundingClientRect().top
+        const currentContainer = document.querySelector(currentCategory)
+        const projectContainer = document.querySelector("#featured-project")
+
+        if (currentCategory === '#featured-project') {
+            gsap.to(window, {
+                scrollTo: { y: 0, autoKill: false },
+                duration: 1
+            })
+            return; // Exit early, skipping the timeline
+        }
+
+        var tl = gsap.timeline()
+        tl
+            .to(currentContainer, {
+                onStart: function () {
+                    gsap.to(window, {
+                        scrollTo: { y: 0, autoKill: false },
+                        duration: 1
+                    })
+                },
+                opacity: 0,
+                ease: "power3.out",
+                duration: .5,
+                onComplete: function () {
+                    gsap.set(currentContainer, { display: "none" })
+                }
+            })
+            .to(projectContainer, {
+                opacity: 1,
+                ease: "power3.out",
+                duration: .5,
+                onStart: function () {
+                    gsap.set(projectContainer, { display: "flex" })
+                },
+                onComplete: function () {
+                    currentCategory = "#featured-project"
+                }
+            })
+    })
+
 }
-filterAnimation() 
+filterAnimation()
 
 function projectAnimation() {
     document.querySelectorAll(".project").forEach(function (project) {
@@ -216,14 +378,5 @@ function footerAnimation() {
     updateTime();
     setInterval(updateTime, 60000);
 
-
-    document.querySelector(".rt-btm input").addEventListener("focus", function () {
-        document.querySelector("#input-placeholder").style.display = "none";
-    })
-    document.querySelector(".rt-btm input").addEventListener("blur", function (e) {
-        if (e.target.value == "") {
-            document.querySelector("#input-placeholder").style.display = "block";
-        }
-    })
 }
 footerAnimation()
